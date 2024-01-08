1ffebca66a543601cc0af492aa1db72d435fa8a8
notzree <notzree@gmail.com>
2024-01-08 16:13:18 -0500

    add auth

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,22 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-
+import { JWT, getToken } from "next-auth/jwt"
 const protectedRoutes = ["/dashboard"];
 const publicRoutes = ["/register", "/"];
-export function middleware(request: NextRequest) {
-  var temp = request.cookies.get("currentUser")?.value;
-    const currentUser = temp?.substring(1, temp.length-1) ;
-  if (
-    protectedRoutes.includes(request.nextUrl.pathname) &&
-    (!currentUser ||Date.now()/1000 > parseInt(JSON.parse(currentUser as string).exp) )) {
-       //since Date.Now gives in ms, exp is in sec
-    request.cookies.delete("currentUser");
-    const response = NextResponse.redirect(new URL("/", request.url));
-    response.cookies.delete("currentUser");
-
-    return response;
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request })
+  if (protectedRoutes.includes(request.nextUrl.pathname)) {
+    if (!token || isTokenExpired(token)) {
+      const response = NextResponse.redirect(new URL("/", request.url)); // modify this later
+      return response;
+    }
+    
   }
+
+}
+
+
+const isTokenExpired = (token: JWT) => {
+  const currentTime = Math.floor(Date.now() / 1000); // Convert to seconds
+  return currentTime > (token.exp as number) ;
 }
